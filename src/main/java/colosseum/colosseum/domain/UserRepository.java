54e0ca8d2756af0a12cdbc0b450151a14ec85047
jhon3242 <rldotnr2@gmail.com>
2023-05-22 17:30:34 +0900

    feat (domain) : 유저 및 저장소 추가

---
+++ src/main/java/colosseum/colosseum/domain/UserRepository.java
@@ -0,0 +1,28 @@
+package colosseum.colosseum.domain;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class UserRepository {
+	private final Map<Long, User> repository = new HashMap<>();
+	private Long sequence = 0L;
+
+	public User save(User user) {
+		user.setId(sequence++);
+		repository.put(user.getId(), user);
+		return user;
+	}
+
+	public User findById(Long id) {
+		return repository.get(id);
+	}
+
+	public List<User> finaAll() {
+		return new ArrayList<>(repository.values());
+	}
+}
