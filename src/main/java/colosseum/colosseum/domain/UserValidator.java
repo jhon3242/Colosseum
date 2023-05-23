c6ccfc52a5e11297d8aac7d5ca801e0424018eb7
jhon3242 <rldotnr2@gmail.com>
2023-05-23 16:02:22 +0900

    feat (docs) : 유저 및 저장소 추가

--- src/main/java/colosseum/colosseum/domain/UserValidator.java
+++ src/main/java/colosseum/colosseum/domain/UserValidator.java
@@ -4,24 +4,24 @@ import org.springframework.stereotype.Controller;
 
 import java.util.Objects;
 
-@Controller
-public class UserValidator {
-
-	private static UserRepository userRepository;
-
-	public static void validateSave(User user) {
-		try {
-			validateSameEmail(user);
-		} catch (Exception err) {
-			System.out.println("Error : "  + err);
-		}
-	}
-
-	private static void validateSameEmail(User user) {
-		int count = (int) userRepository.finaAll()
-				.stream()
-				.filter(tmpUser -> Objects.equals(tmpUser.getEmail(), user.getEmail()))
-				.count();
-		if (count > 0) throw new IllegalArgumentException("동일한 이메일이 있습니다.");
-	}
-}
+//@Controller
+//public class UserValidator {
+//
+//	private static UserRepository userRepository;
+//
+//	public static void validateSave(User user) {
+//		try {
+//			validateSameEmail(user);
+//		} catch (Exception err) {
+//			System.out.println("Error : "  + err);
+//		}
+//	}
+//
+//	private static void validateSameEmail(User user) {
+//		int count = (int) userRepository.finaAll()
+//				.stream()
+//				.filter(tmpUser -> Objects.equals(tmpUser.getEmail(), user.getEmail()))
+//				.count();
+//		if (count > 0) throw new IllegalArgumentException("동일한 이메일이 있습니다.");
+//	}
+//}
