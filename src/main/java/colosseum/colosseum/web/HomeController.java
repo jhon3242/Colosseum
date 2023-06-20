7521e6141abe929d26f08e4cfaa00d4596b1f5d2
jhon3242 <rldotnr2@gmail.com>
2023-06-20 12:46:18 +0900

    feat : 국제화 구현 messages_en.properties 의 추가로 국제화 구현했다.

--- src/main/java/colosseum/colosseum/web/HomeController.java
+++ src/main/java/colosseum/colosseum/web/HomeController.java
@@ -2,11 +2,13 @@ package colosseum.colosseum.web;
 
 import colosseum.colosseum.SessionConst;
 import colosseum.colosseum.domain.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+@Slf4j
 @Controller
 public class HomeController {
 
@@ -14,9 +16,11 @@ public class HomeController {
 	public String mainPage(@SessionAttribute(value = SessionConst.LOGIN_USER, required = false) User user,
 	                       Model model){
 		if (user == null) {
+			log.info("미 로그인 유저");
 			return "home";
 		}
 		model.addAttribute("user", user);
+		log.info("로그인 유저");
 		return "loginHome";
 	}
 }
