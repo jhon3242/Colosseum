1297ed4a294daf24b97e59ea34f93069e65d3eee
jhon3242 <rldotnr2@gmail.com>
2023-07-16 19:32:58 +0900

    feat(Post) : Post 작성 페이지 구현 및 새글 작성 버튼과 연결

--- src/main/java/colosseum/colosseum/web/post/PostController.java
+++ src/main/java/colosseum/colosseum/web/post/PostController.java
@@ -7,8 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @Slf4j
@@ -26,5 +30,14 @@ public class PostController {
 		return "post/postList";
 	}
 
+	@PostMapping
+	public String newPostRedirect(HttpServletResponse response) throws IOException {
+		response.sendRedirect("/post/new");
+		return "redirect:/post/";
+	}
 
+	@GetMapping("new")
+	public String newPostPage() {
+		return "/post/newPost";
+	}
 }
