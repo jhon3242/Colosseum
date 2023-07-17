55c17daf8121a7250993c86cb99c9f41ac4edbd7
jhon3242 <rldotnr2@gmail.com>
2023-07-17 15:25:23 +0900

    feat(Post) : 게시판 상세 페이지 구현

--- src/main/java/colosseum/colosseum/web/post/PostController.java
+++ src/main/java/colosseum/colosseum/web/post/PostController.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -29,7 +26,7 @@ public class PostController {
 
 	@GetMapping
 	public String postList(Model model) {
-		log.info("post 페이지 요청");
+		log.info("post 리스트 요청");
 		model.addAttribute("posts", postRepository.findAll());
 		return "post/postList";
 	}
@@ -54,11 +51,28 @@ public class PostController {
 
 	@PostMapping("new")
 	public String newPost(@ModelAttribute Post post, HttpServletRequest request) {
+		setPostInfo(post, request);
+		log.info("post 새 글 작성 = {}", post);
+		return "redirect:/post";
+	}
+
+	private void setPostInfo(Post post, HttpServletRequest request) {
 		post.setCreatedDate(LocalDateTime.now());
 		post.setModifiedDate(LocalDateTime.now());
 		post.setAuthor(getAuthor(request));
 		postRepository.save(post);
-		log.info("post 새 글 작성 = {}", post);
-		return "redirect:/post";
 	}
+
+	@GetMapping("{postId}")
+	public String postPage(@PathVariable Long postId, Model model) {
+		Post post = postRepository.findById(postId);
+		if (post == null) {
+			log.error("post not found");
+			return "redirect:/post";
+		}
+		log.info("post 페이지 요청 결과 = {}", post);
+		model.addAttribute("post", post);
+		return "/post/postPage";
+	}
+
 }
