34a52c8879166bdb9aff4d8061097204c7a2f70c
jhon3242 <rldotnr2@gmail.com>
2023-07-17 15:43:53 +0900

    feat(Post) : 게시글 ID 포맷터 적용

--- src/main/java/colosseum/colosseum/domain/post/Post.java
+++ src/main/java/colosseum/colosseum/domain/post/Post.java
@@ -2,11 +2,13 @@ package colosseum.colosseum.domain.post;
 
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
 
 import java.time.LocalDateTime;
 
 @Data
 public class Post {
+	@NumberFormat(pattern = "000000")
 	private Long postId;
 	private String title;
 	private String content;
