586fa7fbb961efb7617f5f3b3d4825eba5a4ecf8
jhon3242 <rldotnr2@gmail.com>
2023-07-17 16:14:14 +0900

    feat(Post) : 이미지 파일 추가를 위한 도메인 생성

---
+++ src/main/java/colosseum/colosseum/domain/file/UploadFile.java
@@ -0,0 +1,9 @@
+package colosseum.colosseum.domain.file;
+
+import lombok.Data;
+
+@Data
+public class UploadFile {
+	private String storeFileName;
+	private String uploadFileName;
+}
--- src/main/java/colosseum/colosseum/domain/post/Post.java
+++ src/main/java/colosseum/colosseum/domain/post/Post.java
@@ -1,10 +1,13 @@
 package colosseum.colosseum.domain.post;
 
+import colosseum.colosseum.domain.file.UploadFile;
 import lombok.Data;
+import org.springframework.core.io.Resource;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 public class Post {
@@ -13,6 +16,7 @@ public class Post {
 	private String title;
 	private String content;
 	private String author;
+	private List<UploadFile> images;
 
 	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	private LocalDateTime createdDate;
